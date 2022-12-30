83116b3f9afaebeb3d94ef6958123c5ff5b574ca
Craig Bucher <97258442+craigbucher@users.noreply.github.com>
2022-12-29 20:08:58 -0500

    course complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+// React with TypeScript Crash Course: https://www.youtube.com/watch?v=jrKcJxF0lAU
+
+import React, { useState } from 'react';
 import './App.css';
+import List from './components/list'
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+      name: string
+      age: number
+      url: string
+      note?: string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      age: 35,
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/977.png"
+    }
+  ])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
+      {/* passing prop 'people' defined as the variable 'people' in useState */}
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
